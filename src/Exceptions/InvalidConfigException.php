fb598686b88f0d41cd797544f34158a52619ff35
chunrongl <chunrongl12@outlook.com>
2019-06-12 13:39:52 +0800

    优化190612-001

--- src/Exceptions/InvalidConfigException.php
+++ src/Exceptions/InvalidConfigException.php
@@ -3,8 +3,4 @@ namespace Chunrongl\TqigouRpcClient\Exceptions;
 
 class InvalidConfigException extends \Exception
 {
-    public function __construct($message, $raw = [], $code = 2)
-    {
-        parent::__construct($message, $raw, $code);
-    }
 }
